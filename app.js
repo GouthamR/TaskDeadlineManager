c5571de88d08e15e02ddd1487809743466fb341c
GouthamR <gouthamrajeev1+github@gmail.com>
2016-10-02 12:04:37 -0700

    Allow port override.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 
+app.set("port", process.env.PORT || 3000);
+
 app.get('/', function(req, res)
 {
 	res.send("Index goes here.");
@@ -11,7 +13,7 @@ app.get('/calendar', function(req, res)
 	res.send("Calendar goes here.");
 });
 
-var server = app.listen(3000, function()
+var server = app.listen(app.get("port"), function()
 {
-	console.log("Listening...");
+	console.log("Listening on port " + app.get("port"));
 });
