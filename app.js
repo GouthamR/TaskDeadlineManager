36866f1216f7abc8e947f3490929d0887f00f793
GouthamR <gouthamrajeev1+github@gmail.com>
2016-10-02 12:17:50 -0700

    Use text/plain for index and calendar pages.

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ app.set("port", process.env.PORT || 3000);
 
 app.get('/', function(req, res)
 {
+	res.type("text/plain");
 	res.send("Index goes here.");
 });
 
 app.get('/calendar', function(req, res)
 {
+	res.type("text/plain");
 	res.send("Calendar goes here.");
 });
 
