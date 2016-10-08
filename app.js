2f590eeafaf97f702a7ed39c2bf2050fdd4e50f3
GouthamR <gouthamrajeev1+github@gmail.com>
2016-10-08 12:33:28 -0700

    Add basic views and main template.

--- app.js
+++ app.js
@@ -2,34 +2,33 @@ var express = require("express");
 
 var app = express();
 
+var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set("port", process.env.PORT || 3000);
 
 app.get('/', function(req, res)
 {
-	res.type("text/plain");
-	res.send("Index goes here.");
+	res.render("index");
 });
 
 app.get('/calendar', function(req, res)
 {
-	res.type("text/plain");
-	res.send("Calendar goes here.");
+	res.render("calendar");
 });
 
 app.use(function(req, res)
 {
-	res.type("text/plain");
 	res.status(404);
-	res.send("404 - Not Found");
+	res.render("404");
 });
 
 app.use(function(err, req, res, next)
 {
 	console.error(err.stack);
-	res.type("text/plain");
 	res.status(500);
-	res.send("500 - Server Error");
-
+	res.render("500");
 });
 
 var server = app.listen(app.get("port"), function()
