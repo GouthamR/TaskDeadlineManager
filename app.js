2dabc7ad7dba7ca8003dd67d7f057523dd95fe72
GouthamR <gouthamrajeev1+github@gmail.com>
2016-10-29 11:47:51 -0700

    Move server task and deadlines JSON into files.

--- app.js
+++ app.js
@@ -38,87 +38,14 @@ app.get('/calendar', function(req, res)
 app.get('/load-tasks', function(req, res)
 {
 	// STUB:
-
-	var tasks = 
-	[
-		{
-			"title":"Clean Room",
-			"startEpochMillis": "1477683051161", 
-			"endEpochMillis": "1477686651161",
-			"isAllDay":"false"
-		},
-		{
-			"title":"Math HW",
-			"startEpochMillis": "1477709571161", 
-			"endEpochMillis": "1477709571161",
-			"isAllDay":"false"
-		},
-		{
-			"title":"Lunch",
-			"startEpochMillis": "1477702851161", 
-			"endEpochMillis": "1477722651161",
-			"isAllDay":"false"
-		},
-		{
-			"title":"End Poverty",
-			"startEpochMillis": "1477709571161", 
-			"endEpochMillis": "1477709571161",
-			"isAllDay":"true"
-		},
-		{
-			"title":"Game of Thrones Marathon",
-			"startEpochMillis": "1477708251161", 
-			"endEpochMillis": "1477711851161",
-			"isAllDay":"false"
-		},
-		{
-			"title":"Solve the Water Stagnation Problem",
-			"startEpochMillis": "1477708251161", 
-			"endEpochMillis": "1477711851161",
-			"isAllDay":"true"
-		},
-		{
-			"title":"Dinner",
-			"startEpochMillis": "1477708251161", 
-			"endEpochMillis": "1477711851161",
-			"isAllDay":"false"
-		}
-	];
+	var tasks = require("./tasks.json");
 	res.json(tasks);
 });
 
 app.get('/load-deadlines', function(req, res)
 {
 	// STUB:
-
-	var deadlines = 
-	[
-		{
-			"title": "English Paper",
-			"startEpochMillis": "1477711778000",
-			"isAllDay": "false"
-		},
-		{
-			"title": "Game of Thrones Seasons 1-8 Due",
-			"startEpochMillis": "1460462400000",
-			"isAllDay": "false"
-		},
-		{
-			"title": "Math HW Due",
-			"startEpochMillis": "1474329600000",
-			"isAllDay": "true"
-		},
-		{
-			"title": "Cure to Cancer Due",
-			"startEpochMillis": "1480032000000",
-			"isAllDay": "true"
-		},
-		{
-			"title": "Spaces vs Tabs Rant Post Deadline",
-			"startEpochMillis": "1483315200000",
-			"isAllDay": "true"
-		}
-	];
+	var deadlines = require("./deadlines.json");
 	res.json(deadlines);
 });
 
