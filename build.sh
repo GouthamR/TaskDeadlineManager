ec1fbb6bf0fa57462af5e1acd444d382a00bfeb2
GouthamR <gouthamrajeev1+github@gmail.com>
2016-12-13 20:39:47 -0800

    Modify build script to browserify nav.js. For consistency, even if not strictly necessary.

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 cd client-ts 
 echo compiling typescript...
 tsc *.ts
-mv nav.js ../public/js/
 echo done
 echo browserifying...
 browserify index.js -o ../public/js/index-bundle.js
 browserify calendar.js -o ../public/js/calendar-bundle.js
 browserify add-task.js -o ../public/js/add-task-bundle.js
+browserify nav.js -o ../public/js/nav-bundle.js
 echo done
